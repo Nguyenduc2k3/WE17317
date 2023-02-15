bf5fc9458309156da447b80af4bfa35094e04736
Nguyenduc2k3 <89559446+Nguyenduc2k3@users.noreply.github.com>
2023-02-15 15:20:03 +0700

    useState and useEffect

---
+++ WE17317/src/main.js
@@ -0,0 +1,20 @@
+import { render, router } from "../lib"
+import HomePage from "./pages/home"
+import ProductPage from "./pages/products"
+import './style/main.css'
+
+// DOM declaration
+var app = document.querySelector('#app')
+
+router.on('/', function() {
+    render(HomePage, app)
+})
+
+router.on('/products/:id', function({data}) {
+    render(ProductPage(data.id), app)
+})
+
+router.resolve()
+
+
+
